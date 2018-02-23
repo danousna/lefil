67a3ed3bc4c7631fc3ec5206d313869e6908b7bb
Natan Danous <natan.danous@gmail.com>
2018-02-23 10:01:55 +0100

    Ajout page de maintenance.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Maintenance</title>
+    <style type="text/css" media="screen">
+        body {
+            font-size: 20px;
+            color: black;
+            font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-weight: 800;
+        }
+
+        h3, h4, h5, h5 {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        }
+        .main {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <h3>Application en maintenance, revenez plus tard.</h3>
+    </div>
+</body>
+</html>
