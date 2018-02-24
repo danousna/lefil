08ccab3de7a2381b11dcb683d3cd7f7e7e9f4b8f
Natan Danous <natan.danous@gmail.com>
2018-02-24 13:56:21 +0100

    Finition de la recherche par Algolia

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,41 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Article extends Model
 {
+    use Searchable;
+
+    public function isPublished()
+    {
+        return $this->status == "published";
+    }
+
+    public function shouldBeSearchable()
+    {
+        return $this->isPublished();
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $record = $this->toArray();
+
+        $date = explode('-', substr($record['created_at'], 0, 10));
+        $url = url('/') .'/'. $date[0] .'/'. $date[1] .'/'. $date[2] .'/'. $record['slug'];
+
+        $record['url'] = $url;
+
+        unset($record['id'], $record['user_id'], $record['category_id'], $record['issue_id'], $record['image'], $record['status'], $record['anonymous'], $record['created_at'], $record['updated_at']);
+
+        return $record;
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -1,4 +1,10 @@
 <!-- Bootstrap core JavaScript -->
 {{-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script> --}}
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
+<script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+<script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+
 <script src="{{ asset('js/base.js') }}"></script>
+<script src="{{ asset('js/search.js') }}"></script>
