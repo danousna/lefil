bce74a4d9c59ab3677c1000d35b9982758157270
Natan Danous <natan.danous@gmail.com>
2018-02-26 23:40:50 +0100

    Ajout pdf + thumbs aux numéros

--- resources/views/issues/create.blade.php
+++ resources/views/issues/create.blade.php
@@ -8,7 +8,7 @@
 <div class="container-fluid">
     <div class="row py-4 bg-bubble">
         <div class="div-bubble col-xl-6 col-lg-8 col-11 mx-auto my-4 p-4">
-            <form method="POST" action="{{ route('issues.store') }}">
+            <form method="POST" action="{{ route('issues.store') }}" enctype="multipart/form-data">
                 {{ csrf_field() }}
 
                 <div class="form-group">
@@ -26,6 +26,14 @@
                     <input type="date" name="release_date" class="form-control" value="{{ old('release_date') }}" required>    
                 </div>
 
+                <div class="form-group file-input">
+                    <label for="pdf">Fichier pdf du numéro <small class="text-muted">(Vous pouvez aussi l'ajouter plus tard)</small></label>
+                    <br>
+                    <input type="file" name="pdf" id="pdf">
+                    <span class="btn btn-secondary mr-3">Choisir le fichier</span>
+                    <span class="label" data-js-label>Aucun ficher sélectionné</label>
+                </div>
+
                 <br>
 
                 <input type="submit" value="Créer" class="btn btn-primary">
@@ -33,5 +41,11 @@
         </div>
     </div>
 </div>
+
+@endsection
+
+@section('scripts')
+    
+    <script src="{{ asset('js/file_input.js') }}"></script>
 
 @endsection
