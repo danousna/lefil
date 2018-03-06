cd461de3dbe52a90505316b7ff03cc06ca079ff9
Natan Danous <natan.danous@gmail.com>
2018-03-06 22:52:26 +0100

    Traduction erreurs, design fixes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,26 +1,25 @@
 <!-- Footer -->
 <footer>
-    <div class="container px-4">
-        <div class="row">
-            <div class="col-sm py-4">
+    <div class="container">
+        <div class="row py-4">
+            <div class="col-md-6 mb-4">
                 <div class="text-center logo">
-                    <h4><a class="title" href="/"><img style="width: 120px" src="{{ asset('Logo-gray.svg') }}"></a></h4>
+                    <img style="width: 70px" src="{{ asset('Logo-gray.svg') }}">
                 </div>
                 <h6 class="mt-3 title">Le journal des étudiants de l'UTC</h6>
                 <span class="about">Le Fil est le torchon bimensuel gratuit des étudants tiré à 600 exemplaires. Chaque article n'engage que la responsabilité de son auteur.</span>
             </div>
-            <div class="col-sm py-4">
-                <h6>Plan du site</h6>          
+            <div class="col-md-6">     
                 <div class="row">
-                   <div class="col-sm"> 
-                        <small class="d-block mb-2 mt-3">Découvrir :</small>
+                   <div class="col-6"> 
+                        <small class="d-block title mb-2">Découvrir :</small>
                         <a href="/rubriques" class="about site-map-link">Rubriques</a><br>
                         <a href="/numéros" class="about site-map-link">Numéros</a><br>
                         <a href="/bops" class="about site-map-link">Bops</a><br>
                         <a href="/spotted" class="about site-map-link">Spotted</a>
                     </div>
-                    <div class="col-sm">                     
-                        <small class="d-block mb-2 mt-3">L'association :</small>
+                    <div class="col-6">                     
+                        <small class="d-block title mb-2">L'association :</small>
                         <a href="/about" class="about site-map-link">À propos</a><br>
                         <a href="/contact" class="about site-map-link">Contact</a><br>
                         <a href="/archive" class="about site-map-link">Archive</a><br>
@@ -29,13 +28,6 @@
                         @endif
                     </div>
                 </div>
-            </div>
-            <div class="col-sm">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col pb-3">
-                <span class="about">Le Fil est une association étudiante de l'UTC.</span>
             </div>
         </div>
     </div>
